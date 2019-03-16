037ba5836678a55dbe1722f036d622932b003633
Martin <martin@Keynes.local>
2019-03-16 12:12:54 +0100

    Initial commit

---
+++ static/mail_handler.php
@@ -0,0 +1,39 @@
+<?php
+
+$EmailFrom = "mceo@mailbox.org";
+$EmailTo = "mceo@mailbox.org";
+$Subject = "Contact Form Test";
+$name = Trim(stripslashes($_POST['name'])); 
+$email = Trim(stripslashes($_POST['email'])); 
+$message = Trim(stripslashes($_POST['message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=404.html\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=index.html\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=404.html\">";
+}
+?>
